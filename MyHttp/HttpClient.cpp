e6caed3129782f075302f7ff0891c4b674e8b884
liehuoe <397380372@qq.com>
2017-11-09 22:11:59 +0800

    Update HttpClient.cpp
    解决session释放的问题

--- MyHttp/HttpClient.cpp
+++ MyHttp/HttpClient.cpp
@@ -6,7 +6,6 @@ void HttpClient::Release()
 {
 	if (m_hRequest) { WinHttpCloseHandle(m_hRequest); m_hRequest = NULL; }
 	if (m_hConnect) { WinHttpCloseHandle(m_hConnect); m_hConnect = NULL; }
-	if(m_hSession) { WinHttpCloseHandle(m_hSession); m_hSession = NULL; }
 }
 
 BOOL HttpClient::SetRequest(LPCWSTR szURL, REQUEST_TYPE type)
@@ -26,19 +25,18 @@ BOOL HttpClient::SetRequest(LPCWSTR szURL, REQUEST_TYPE type)
 	urlCom.dwUrlPathLength = sizeof(szUrlPath) / sizeof(WCHAR);
 	if(!WinHttpCrackUrl(strURL.c_str(), strURL.length(), ICU_ESCAPE, &urlCom)) return FALSE;
 
-	HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
+	HINTERNET hConnect = NULL, hRequest = NULL;
 	do 
 	{
-		if(!(hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0))) break;
-		if(!(hConnect = WinHttpConnect(hSession, urlCom.lpszHostName, urlCom.nPort, 0))) break;
+		if(!m_hSession && !(m_hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0))) break;
+		if(!(hConnect = WinHttpConnect(m_hSession, urlCom.lpszHostName, urlCom.nPort, 0))) break;
 		if(!(hRequest = WinHttpOpenRequest(hConnect, type == REQUEST_TYPE_POST ? L"POST" : L"GET", urlCom.lpszUrlPath, NULL, WINHTTP_NO_REFERER, 
 			WINHTTP_DEFAULT_ACCEPT_TYPES, urlCom.nScheme == INTERNET_SCHEME_HTTPS ? WINHTTP_FLAG_SECURE : 0))) break;
 	} while (0);
 
-	if(hSession && hConnect && hRequest)
+	if(hConnect && hRequest)
 	{
 		Release();
-		m_hSession = hSession;
 		m_hConnect = hConnect;
 		m_hRequest = hRequest;
 		return TRUE;
@@ -57,11 +55,11 @@ BOOL HttpClient::AddHeader(LPCWSTR szHeader)
 
 BOOL HttpClient::Send(LPCSTR szPostData, int len)
 {
-	if(!szPostData || len == 0)
+	if(!szPostData)
 		return WinHttpSendRequest(m_hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, 0, 0, 0, 0);
 	else
 	{
-		if(len == 0) len = strlen(szPostData);
+		if(len <= 0) len = strlen(szPostData);
 		return WinHttpSendRequest(m_hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, (LPVOID)szPostData, len, len, 0);
 	}
 }
